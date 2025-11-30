a906ee22ad579844d42337e9fe1b6ab1e790d3f5
Monique Huynh <47400590+mhxynh@users.noreply.github.com>
2025-11-30 15:18:36 -0500

    Updated navbar

--- src/components/CoreNavbar.vue
+++ src/components/CoreNavbar.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRouter } from 'vue-router'
+
+const router = useRouter();
+const isLoggedIn = localStorage.getItem('isLoggedIn') == 'true';
+
+const logoutButton = () => {
+  localStorage.removeItem('account_id');
+  localStorage.setItem('isLoggedIn', 'false');
+  router.push('/');
+  setTimeout(() => {
+    window.location.reload();
+  }, 50);
+};
 </script>
 
 <template>
@@ -10,7 +22,23 @@ import { RouterLink } from 'vue-router'
         <h2 class="navbar__title">Sticker Shop</h2>
         <RouterLink to="/" class="nav-link fs-4">Home</RouterLink>
       </div>
-      <div>
+      <div class="icons">
+        <template v-if="isLoggedIn">
+          <RouterLink to="/account" class="nav-link fs-4">
+            <FontAwesomeIcon icon="user-circle"/>
+          </RouterLink>
+          <a @click.prevent="logoutButton" href="#" class="nav-link fs-4">
+            <FontAwesomeIcon icon="sign-out-alt"/>
+          </a>
+        </template>
+        <template v-else>
+          <RouterLink to="/signup" class="nav-link fs-4">
+            <FontAwesomeIcon icon="user-plus"/>
+          </RouterLink>
+          <RouterLink to="/login" class="nav-link fs-4">
+            <FontAwesomeIcon icon="sign-in-alt"/>
+          </RouterLink>
+        </template>
         <RouterLink to="/cart" class="nav-link fs-4">
           <FontAwesomeIcon icon="cart-shopping"/>
         </RouterLink>
@@ -22,5 +50,9 @@ import { RouterLink } from 'vue-router'
 <style scoped lang="scss">
 .navbar__title {
   margin-right: 32px;
+}
+.icons {
+  display: flex;
+  gap: 10px;
 }
 </style>
